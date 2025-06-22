c740dbfb47c4134671100a0fb4f6c6a0a1aae4c3
Ori <odmark2023@gmail.com>
2025-06-22 09:24:33 +0300

    temp file not sure if i should use it

--- date_and_time.py
+++ date_and_time.py
@@ -1,10 +1,20 @@
 import datetime
+import threading
 
+timer_running = True
 def get_time():
-    return datetime.datetime.now()
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-def compare_time():
-    time = datetime.datetime.now()
-    return time > get_time()
+
+def timer():
+    if timer_running:
+        time = threading.Timer(10.0, timer)
+        time.start()
+
+
+
+def stop_timer():
+    global timer_running
+    timer_running = False
 
 
