a7f0ee19e1808427ea2dae31788d3a74b16a7a12
Ori <odmark2023@gmail.com>
2025-06-26 09:48:50 +0300

    add commits to explain the functions and active shortcut button and print file

--- manager.py
+++ manager.py
@@ -14,52 +14,90 @@ class Manager:
     """
     start the thread listener and the timer 
     """
+
     def start(self):
         self.key_listener.start_run_and_listener()
+
         self.new_thread()
-        # if self.key_listener.temp_fun():
-        #     self.stop()
+
         self.key_listener.start_join()
+
     """
     timer for 60 sec that will execute the append fun
     """
 
     def timer(self):
         if self.running:
-            self.append_dic()
-            time = threading.Timer(60.0, self.timer)
+            if self.key_listener.key_holder:
+                self.append_dic()
+
+            time = threading.Timer(5.0, self.timer)
             time.start()
+
     """
     update the dictionary with the time and key pressing
     and write it to file and restart the dictionary
     """
+
     def append_dic(self):
+        self.exit_button()
+
         press = self.key_listener.hold_press()
+
+        self.show_fun()
+
         self.dic.update({date_and_time.get_time(): press})
         print(self.dic)
-        self.file.write_to_file(str(self.dic))
+
+        self.file.write_to_file(str(self.dic) + "\n\n")
+
+        self.clear()
+
+    def clear(self):
         self.key_listener.key_holder = ""
         self.dic.clear()
 
     """
+    print the file with all the pressing
+    """
+
+    def show_fun(self):
+        if self.key_listener.key_holder.endswith("show"):
+            self.call_read_file()
+
+    """
+    call the function to finish the program
+    """
+
+    def exit_button(self):
+        if self.key_listener.close_button:
+            self.clear()
+            self.stop()
+
+    """
     new thread for the timer to run on the same time with the listener
     """
+
     def new_thread(self):
         timer_thread = threading.Thread(target=self.timer)
         timer_thread.daemon = True
         timer_thread.start()
+
     """
     print every thing that was written until now 
     """
+
     def call_read_file(self):
         read = self.file.read_file()
         print(read)
+
     """
     stop the program
     """
+
     def stop(self):
         self.call_read_file()
+
         self.key_listener.stop_program()
-        date_and_time.stop_timer()
 
-
+        self.running = False
