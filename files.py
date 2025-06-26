e326d5c1fd82fe2b8ff0cf24b26db106ff9948c1
Ori <odmark2023@gmail.com>
2025-06-26 09:47:45 +0300

    add commits to explain the functions

--- files.py
+++ files.py
@@ -3,6 +3,10 @@ class MyFile:
         self.file_name = file_name
         self.create_or_check_file()
 
+    """
+    check if file exist and create new if not
+    """
+
     def create_or_check_file(self):
         try:
             with open(self.file_name, "x"):
@@ -11,14 +15,19 @@ class MyFile:
             pass
         return self
 
+    """
+    write to file 
+    """
+
     def write_to_file(self, input_key):
         with open(self.file_name, "a") as file:
             file.write(input_key + " ")
         return self
 
+    """
+    allow to read the file 
+    """
+
     def read_file(self):
         with open(self.file_name, "r") as file:
             return file.read()
-
-
-
